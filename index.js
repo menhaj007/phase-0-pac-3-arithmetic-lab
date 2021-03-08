5fc04c126b76958e256c77171f59cda664b5a4f7
menhaj <menhaj007@yahoo.com>
2021-03-07 19:23:32 -0500

    Complete assignment

---
+++ index.js
@@ -0,0 +1,43 @@
+function add() {
+
+}
+function subtract() {
+
+}
+function multiply() {
+
+}
+
+function divide() {
+    
+}
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a-b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a/b;
+}
+function increment(n) {
+    n++;
+    return n;
+}
+function decrement(n) {
+    n--;
+    return n;
+} 
+function makeInt(n) {
+    return parseInt(n);
+}
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+    n = parseFloat(n);
+    return n;
+}
